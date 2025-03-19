aef0a8972b24d908f037d7668eef065238feff75
Andressa <henning.andressa1@gmail.com>
2025-03-19 15:11:34 -0300

    testando commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
